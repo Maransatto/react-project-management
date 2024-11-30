94fa2b1dcc632dae9db1b77078b3a433478e2b65
Fernando Silva Maransatto <fernando.maransatto@gmail.com>
2024-11-30 12:33:46 -0300

    feat: 150. Adding the "New Project" component & A Reusable "Input" Component

---
+++ src/components/ProjectsSidebar.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+
+export default function ProjectsSidebar() {
+  const [projects, setProject] = useState([]);
+
+  function handleAddProject() {}
+  return (
+    <aside className="w-1/3 px-8 py-16 bg-stone-900 text-stone-50 md:w-72 rounded-r-xl">
+      <h2 className="mb-8 font-bold uppercase md:text-xl text-stone-200">
+        Your Projects
+      </h2>
+      <div>
+        <button className="px-4 py-2 text-xs md:text-base rounded-md bg-stone-700 text-stone-400 hover:bg-stone-600 hover:text-stone-100">
+          + Add Project
+        </button>
+      </div>
+      <ul className="mt-8">
+        <button className="w-full text-left px-2 py-1 rounded-sm my-1 hover:text-stone-200 hover:bg-stone-800">
+          Learning React
+        </button>
+        <button className="w-full text-left px-2 py-1 rounded-sm my-1 hover:text-stone-200 hover:bg-stone-800">
+          Learning React
+        </button>
+      </ul>
+    </aside>
+  );
+}
